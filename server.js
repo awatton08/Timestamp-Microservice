b5d1a81aca9f5d9b5bc58abe656fbae534de2e70
awatton <a.watton08@gmail.com>
2021-04-17 16:55:17 -0400

    sdf

--- server.js
+++ server.js
@@ -24,6 +24,9 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/:date", (req, res) => {
+  res.json({ date : req.params.date })
+});
 
 
 // listen for requests :)
